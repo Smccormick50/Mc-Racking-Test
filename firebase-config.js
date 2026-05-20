7259e38d00f7de2e3efd9657e0a852de75890bed
Smccormick50 <steven.mccormick50@gmail.com>
2026-05-20 16:08:24 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,5 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyCg1Ih-IbxcoPIhAc_7mzJF2bJ9WX0EMyw",
+  apiKey: "AIzaSyCgI1h-IbxcoPIhAc_7mzJF2bJ9WX0EMyw",
   authDomain: "mc-racking---test.firebaseapp.com",
   projectId: "mc-racking---test",
   storageBucket: "mc-racking---test.firebasestorage.app",
@@ -7,3 +7,13 @@ const firebaseConfig = {
   appId: "1:22203078980:web:6547d2250dc75a8d895f11",
   measurementId: "G-43RX41YE28"
 };
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+
+async function ensureSignedIn() {
+  if (!firebase.auth().currentUser) {
+    await firebase.auth().signInAnonymously();
+  }
+}
